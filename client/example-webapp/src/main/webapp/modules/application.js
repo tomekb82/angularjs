78b6eb046441ad3cb814463c3b1e59f4cdc391dc
Tomek <belina.tomasz@gmail.com>
2015-09-04 13:40:47 +0200

    Dodaj brakujaca biblioteke, podnies wersje TypeScript

--- client/example-webapp/src/main/webapp/modules/application.js
+++ client/example-webapp/src/main/webapp/modules/application.js
@@ -4,7 +4,7 @@
  * Bootstraps the example application:
  *  Loads the 'demo' module from 'example-library-js' via RequireJS and instantiates some logic from this library
  */
-require(['example-library-js/demo/Hello'], function(Hello) {
+require(['example-library-js/Hello'], function(Hello) {
 
     var hello = new Hello("test");
 	console.log(hello.name);
