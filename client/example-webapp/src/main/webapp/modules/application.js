c67fad51537786de59c9de32a15aedb9c96e1ebd
Tomek <belina.tomasz@gmail.com>
2015-09-04 13:09:25 +0200

    dodaj test odczytu danych w modelu TS

--- client/example-webapp/src/main/webapp/modules/application.js
+++ client/example-webapp/src/main/webapp/modules/application.js
@@ -1,1 +1,11 @@
 /* insert app code here */
+
+/**
+ * Bootstraps the example application:
+ *  Loads the 'demo' module from 'example-library-js' via RequireJS and instantiates some logic from this library
+ */
+require(['example-library-js/demo/Hello'], function(Hello) {
+
+    var hello = new Hello("test");
+	console.log(hello.name);
+});
