d8195a88de5bab97e4c30ce371b5360bb93301ed
Tomek <belina.tomasz@gmail.com>
2015-09-04 13:56:13 +0200

    Dodaj wstrzsykiwanie modulu Angular - nieudane (!)

--- client/example-webapp/src/main/webapp/modules/application.js
+++ client/example-webapp/src/main/webapp/modules/application.js
@@ -4,8 +4,14 @@
  * Bootstraps the example application:
  *  Loads the 'demo' module from 'example-library-js' via RequireJS and instantiates some logic from this library
  */
-require(['example-library-js/Hello'], function(Hello) {
+require(['example-library-js/Hello', 'example-library-js/Angular'], function(Hello, todos) {
 
     var hello = new Hello("test");
 	console.log(hello.name);
+	
+	var test = todos.todomvc.test;
+	console.log(test);
+	
+	
 });
+
