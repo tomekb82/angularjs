b7278c9c5432e9e7a47bfa6de21e85d5ac7622c3
Tomek <belina.tomasz@gmail.com>
2015-09-04 14:48:17 +0200

    Wylacz testy - DO PRZYWRÓCENIA

---
+++ client/example-library-js/src/main/ts/demo/Angular.ts
@@ -0,0 +1,17 @@
+/// <reference path='_all.ts' />
+
+/**
+ * The main TodoMVC app module.
+ *
+ * @type {angular.Module}
+ */
+export module todos {
+    'use strict';
+
+    var todomvc = angular.module('todomvc', []);
+	todomvc.value("test", "dsds");
+          //  .controller('todoCtrl', TodoCtrl)
+          //  .directive('todoBlur', todoBlur)
+          //  .directive('todoFocus', todoFocus);
+          //  .service('todoStorage', TodoStorage);
+}
--- client/example-webapp/src/main/webapp/modules/application.js
+++ client/example-webapp/src/main/webapp/modules/application.js
@@ -4,14 +4,17 @@
  * Bootstraps the example application:
  *  Loads the 'demo' module from 'example-library-js' via RequireJS and instantiates some logic from this library
  */
-require(['example-library-js/Hello', 'example-library-js/Angular'], function(Hello, todos) {
+require(['example-library-js/Hello'], function(Hello) {
 
     var hello = new Hello("test");
 	console.log(hello.name);
-	
+
+});
+
+require(['example-library-js/Angular'], function(todos) {
+
 	var test = todos.todomvc.test;
 	console.log(test);
-	
 	
 });
 
