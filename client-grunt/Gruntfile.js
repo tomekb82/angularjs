76bede8b33d94098a51893a7dfcd3d11fb6e9b25
Tomasz Belina <tomasz.belina@qualent.eu>
2015-09-13 23:10:01 +0200

    Popraw siezki dla testow

--- client-grunt/Gruntfile.js
+++ client-grunt/Gruntfile.js
@@ -20,8 +20,9 @@ module.exports = function(grunt){
 				}
 			},
 			test:{
-				src: ["app/model/*.ts", "test/specs/*.ts", "app/controllers/controllers.ts", "app/controllers/TestController.ts", "app/controllers/app.ts"],
+				src: ["app/**/*.ts", "test/specs/*.ts"], 
 				reference: "./app/reference.ts" ,				
+				out: 'app/out.js',
 				options: {
 					target: 'es5',
 					sourcemap: 'true',
@@ -31,10 +32,10 @@ module.exports = function(grunt){
 		},
 
 		jasmine : {
-			src : ['app/model/**/*.js', "app/controllers/TestController.js", "app/controllers/app.js"],
+			src : ['app/out.js'],
 			options : {
-				specs : 'test/specs/**/*.js',
-				vendor: ['vendor/types/angular/angular.js', 'vendor/types/angular/angular-mocks.js'],
+				//specs : 'test/specs/**/*.js',
+				vendor: ['vendor/types/angular/angular.js', 'vendor/types/angular/angular-mocks.js', 'vendor/types/at_angular/at_angular.js'],
 				template: require('grunt-template-jasmine-requirejs'),
 				templateOptions: {
 					requireConfig: {
