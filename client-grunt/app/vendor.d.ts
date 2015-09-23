0345899aefcbcf25f0eeffdf9957021b7e71190b
Tomek <belina.tomasz@gmail.com>
2015-09-23 12:50:20 +0200

    Dodaj typowaną bibliotekę jQuery do obsługi pól modalnych

--- client-grunt/app/vendor.d.ts
+++ client-grunt/app/vendor.d.ts
@@ -2,3 +2,5 @@
 /// <reference path='../vendor/types/angular/angular-mocks.d.ts' />
 /// <reference path='../vendor/types/at_angular/at_angular.ts' />
 /// <reference path='../vendor/types/jasmine/jasmine.d.ts' />
+/// <reference path='../vendor/types/jquery/jquery.d.ts' />
+/// <reference path='../vendor/types/jquery/jquery.simplemodal.d.ts' />
