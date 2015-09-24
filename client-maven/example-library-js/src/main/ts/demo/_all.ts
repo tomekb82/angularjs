0c28d25e1a4dbeb436c0f296be6294c22f750273
Tomasz Belina <tomasz.belina@qualent.eu>
2015-09-25 00:39:56 +0200

    Usun niewykorzystywany projekt wykorzystujacy mavena do budowania frontendu

--- client-maven/example-library-js/src/main/ts/demo/_all.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path='jquery.ts' />
-/// <reference path='angular.ts' />
-/// <reference path='at-angular.ts' />
-/// <reference path='AngularApp.ts' />
-
--- client-maven/example-webapp/src/build/js/optimize.js
+++
@@ -1,7 +0,0 @@
-// Optimizer configuration for r.js (see http://requirejs.org/docs/optimization.html)
-({
-    name: "application",
-    baseUrl: "${project.build.directory}/${project.build.finalName}/modules",
-    out: "${project.build.directory}/${project.build.finalName}/modules/application.js",
-    optimize: "uglify"
-})
