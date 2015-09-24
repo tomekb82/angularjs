e8572f7ddb4788e604e748bdf4326804ecf547e3
Tomasz Belina <tomasz.belina@qualent.eu>
2015-09-25 00:35:12 +0200

    Dodaj narzedzia administracyjne

---
+++ client-grunt/app/core/admin/health/health.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('myApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('health', {
+                parent: 'admin',
+                url: '/health',
+                data: {
+                    roles: ['ROLE_ADMIN'],
+                    pageTitle: 'health.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/core/admin/health/health.html',
+                        controller: 'HealthController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('health');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
