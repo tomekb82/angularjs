1d4553e77175fd21abeb58c7e81be3ef5966e4c5
Tomasz Belina <tomasz.belina@qualent.eu>
2015-09-28 19:46:04 +0200

    Odkomentuj anotacjje Audited

--- server/src/main/java/tb/angularjs/model/AbstractAuditingEntity.java
+++ server/src/main/java/tb/angularjs/model/AbstractAuditingEntity.java
@@ -8,7 +8,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//import org.hibernate.envers.Audited;
+import org.hibernate.envers.Audited;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
@@ -20,7 +20,7 @@ import javax.validation.constraints.NotNull;
  * last modified by date.
  */
 @MappedSuperclass
-//@Audited
+@Audited
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractAuditingEntity {
 
